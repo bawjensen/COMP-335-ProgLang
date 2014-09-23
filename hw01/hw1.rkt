cfb32de2f1b7aa421e2893754e2fbdd2eb7eafbe
Bryan Jensen <bryanawjensen@gmail.com>
2014-09-23 10:44:01 -0400

    Some progress, and added assignment pdf

--- hw01/hw1.rkt
+++ hw01/hw1.rkt
@@ -34,3 +34,19 @@
     [numC (n) n]
     [plusC (l r) (+ (interp l) (interp r))]
 ))
+
+(test/exn (parse '(+)) "syntax error")
+(test/exn (parse '(+ 1)) "syntax error")
+(test/exn (parse '(+ 1 2 3)) "syntax error")
+(test/exn (parse '(- 1 2)) "syntax error")
+(test (parse '(+ 1 2)) (plusC (numC 1) (numC 2)))
+(test (parse '(+ 1 (+ 2 3))) (plusC (numC 1) (plusC (numC 2) (numC 3))))
+(test (parse '(+ (+ 1 2) 3)) (plusC (plusC (numC 1) (numC 2)) (numC 3)))
+(test (parse '(+ (+ (+ 1 2) 3) 4)) (plusC (plusC (plusC (numC 1) (numC 2)) (numC 3)) (numC 4)))
+
+(test (interp (plusC (numC 1) (numC 2))) 3)
+(test (interp (parse '(+ 1 2))) 3)
+(test (interp (parse '(+ 1 -2))) -1)
+(test (interp (parse '(+ 1 (+ 2 3)))) 6)
+(test (interp (parse '(+ (+ 1 2) 3))) 6)
+(test (interp (parse '(+ (+ (+ 1 2) 3) 4))) 10)
