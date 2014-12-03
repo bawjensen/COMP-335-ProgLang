fb4be4397b4ca34dcb5514c28d720976a20b94b1
Bryan Jensen <bryanawjensen@gmail.com>
2014-12-02 20:39:27 -0500

    Finished hw3

--- hw03/dshelton_zrubin_bjensen_hw3.rkt
+++ hw03/dshelton_zrubin_bjensen_hw3.rkt
@@ -25,7 +25,7 @@
     [(s-exp-symbol? s) (idC (s-exp->symbol s))] ; symbol -> idC
     [(and (s-exp-list? s)
           (eq? (s-exp->symbol (first (s-exp->list s))) 'begin))
-     (beginC (map parse (rest (s-exp->list s))))]
+     (beginC (map parse (rest (s-exp->list s))))] ; NOTE: Not checking length of rest, assumed to be non-0
     [(and (s-exp-list? s) (= 3 (length (s-exp->list s)))) ; List of length 3 -> ArithC (plusC)
       (let ([sl (s-exp->list s)])
         (case (s-exp->symbol (first sl))
@@ -93,3 +93,44 @@
                          [else (interp a (box (rest (unbox env))))]))]
   )
 )
+
+; Testing parse, with syntax
+(test/exn (parse '(+)) "syntax error")
+(test/exn (parse '(+ 1)) "syntax error")
+(test/exn (parse '(+ 1 2 3)) "syntax error")
+(test (parse '(+ 1 2)) (plusC (numC 1) (numC 2)))
+(test (parse '(+ 1 (+ 2 3))) (plusC (numC 1) (plusC (numC 2) (numC 3))))
+(test (parse '(+ (+ 1 2) 3)) (plusC (plusC (numC 1) (numC 2)) (numC 3)))
+(test (parse '(+ (+ (+ 1 2) 3) 4)) (plusC (plusC (plusC (numC 1) (numC 2)) (numC 3)) (numC 4)))
+
+; Testing interp, with syntax (piggy-backing on parse)
+(test (interp (plusC (numC 1) (numC 2)) (box empty)) 3) ; Test without parse
+(test (interp (parse '(+ 1 2)) (box empty)) 3) ; Test with parse
+(test (interp (parse '(+ 1 -2)) (box empty)) -1)
+(test (interp (parse '(+ 1 (+ 2 3))) (box empty)) 6)
+(test (interp (parse '(+ (+ 1 2) 3)) (box empty)) 6)
+(test (interp (parse '(+ (+ (+ 1 2) 3) 4)) (box empty)) 10)
+
+; Testing addition
+(test (interp (parse '(+ 1 2)) (box empty)) 3) ; Simple addition
+(test (interp (parse '(+ 1 x)) (box (list (bind 'x (box 2))))) 3) ; Addition with id
+
+; Testing declare
+(test (interp (parse '(declare x 5)) (box empty)) 5) ; Valid declare
+(test (interp (parse '(declare x (+ 2 3))) (box empty)) 5) ; Valid declare with sub expression
+(test/exn (interp (parse '(declare x 5)) (box (list (bind 'x (box 4))))) "already exists") ; Invalid declare (already exists)
+
+; Testing set
+(test (interp (parse '(set x 2)) (box (list (bind 'x (box 1))))) 2) ; Valid set
+(test/exn (interp (parse '(set x 2)) (box empty)) "does not exist") ; Invalid set (doesn't already exist)
+
+; Testing begin
+(test (interp (parse '(begin ; Single line begin
+                        (+ 1 2))) (box empty)) 3)
+(test (interp (parse '(begin ; Multi-line begin (last expr's value returned)
+                        (+ 1 2)
+                        (+ 3 y)
+                        (declare x 5)
+                        (set x 6)
+                        (set x (+ x 1))
+                        x)) (box (list (bind 'y (box 4))))) 7)
